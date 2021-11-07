9591f7b840414f176d4aac5e1350fa9f08f46eac
Sam McClure <samkmcclure@gmail.com>
2021-11-07 13:12:22 -0800

    Style pokemon index grid

--- src/components/index/PokemonIndex.tsx
+++ src/components/index/PokemonIndex.tsx
@@ -13,10 +13,40 @@ interface PokemonData {
   url: string,
 }
 
-const DetailContainer = styled.div`
+const IndexContainer = styled.div`
 	padding: 60px;
+	display: grid;
+	grid-template-columns: repeat(5, 1fr);
+	column-gap: 25px;
+	row-gap: 25px;
+
+	@media (max-width: 700px) {
+		grid-template-columns: repeat(3, 1fr);
+	}
+
+	@media (max-width: 400px) {
+		grid-template-columns: 1fr;
+	}
+`;
+
+const PokemonContainer = styled.div`
 	display: flex;
 	flex-direction: column;
+	align-items: center;
+	padding: 10px 0px;
+	border-radius: 8px;
+
+	:hover {
+		background-color: rgba(241, 241, 241, 0.6);
+		transition: all 0.2s ease-in-out;
+		transform: scale(1.05);
+	}
+`;
+
+
+const PokemonLink = styled(Link)`
+	text-decoration: none;
+  color: black;
 `;
 
 const PokemonIndex = (): React.ReactElement => {
@@ -43,18 +73,18 @@ const PokemonIndex = (): React.ReactElement => {
 
   if (pokemonList['data']) {
 		return (
-			<DetailContainer>
+			<IndexContainer>
         {pokemonListCache["data"].map((pokemon: PokemonListItem) => {
           return (
-						<div key={pokemon.id}>
-							<Link to={`pokemon/${pokemon.id}`}>
+						<PokemonLink to={`pokemon/${pokemon.id}`} key={pokemon.id}>
+							<PokemonContainer>
 								<img src={pokemon.imageUrl} alt={pokemon.name} />
 								<div>{pokemon.name}</div>
-							</Link>
-						</div>
+							</PokemonContainer>
+						</PokemonLink>
 					);
         })}
-			</DetailContainer>
+			</IndexContainer>
 		);
 	} else {
 		return <div>Loading</div>;
